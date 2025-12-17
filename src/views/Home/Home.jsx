cb5e9e4ff83f2421fc9084f1f595e2570f530433
Daniel <jilek.d@northeastern.edu>
2025-12-16 20:34:25 -0500

    Working on fixing background image

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,10 +2,11 @@ import './Home.css'
 import Title from '../../components/Title/Title';
 import Logo from '../../components/Logo/Logo';
 import LogIn from '../../components/LogIn/LogIn';
+import imgPath from '../../assets/images/Alhambra_view.png'
 
 function Home() {
     return (
-        <div className="page" id='home-page'>
+        <div className="page" id='home-page' style={{backgroundImage: `${imgPath}`}}>
             <Title></Title>
             <LogIn></LogIn>
         </div>
