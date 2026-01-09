e86da80c2256d7cd4a03a26bbdaa590b62cb2a47
Daniel <jilek.d@northeastern.edu>
2026-01-08 21:50:25 -0500

    Fixed path for board placeholder img

--- src/views/Tablas/Board/Board.jsx
+++ src/views/Tablas/Board/Board.jsx
@@ -2,7 +2,7 @@ import "./Board.css"
 
 function Board() {
     return(
-        <img className="game-board" src="../../src/assets/images/board.png">
+        <img className="game-board" src="../../assets/images/board.png">
 
         </img>
     )
