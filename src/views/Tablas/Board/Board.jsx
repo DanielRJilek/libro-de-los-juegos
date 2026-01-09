a21daf03e509c6882bb1a13b3cd9346376c5560d
Daniel <jilek.d@northeastern.edu>
2026-01-08 21:59:40 -0500

    Fixed path for board img

--- src/views/Tablas/Board/Board.jsx
+++ src/views/Tablas/Board/Board.jsx
@@ -2,7 +2,7 @@ import "./Board.css"
 
 function Board() {
     return(
-        <img className="game-board" src="https://libro-de-los-juegos-server.onrender.com/static/images/doblet.jpg">
+        <img className="game-board" src="https://libro-de-los-juegos-server.onrender.com/static/images/board.png">
 
         </img>
     )
