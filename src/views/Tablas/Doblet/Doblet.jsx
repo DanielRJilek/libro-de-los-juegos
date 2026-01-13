cb5d55f099a9bb1b8a35bc3e78e7b5316c34ebfa
Daniel <jilek.d@northeastern.edu>
2026-01-13 18:32:41 -0500

    Basic websocket

--- src/views/Tablas/Doblet/Doblet.jsx
+++ src/views/Tablas/Doblet/Doblet.jsx
@@ -3,14 +3,29 @@ import Board from "../Board/Board";
 // import Dice from "../Dice/Dice";
 import { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../../../context/AuthContext";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 
 function Doblet() {
     const [board,setBoard] = useState([[2,0,0,2], [2,0,0,2], [2,0,0,2], [2,0,0,2], [2,0,0,2], [2,0,0,2]]);
-    
+    const navigate = useNavigate();
     const params = useParams();
     const instance = params.instance
     const auth = useContext(AuthContext);
+    const ws = new WebSocket('ws://https://libro-de-los-juegos-server.onrender.com:8080');
+    ws.onopen = () => {
+        console.log("Connected to server");
+    }
+    ws.onmessage = (event) => {
+        console.log(event.data)
+    }
+
+    const quit = () => {
+        ws.close = () => {
+            console.log("connection closed")
+            navigate('./games/doblet')
+        }
+    }
+
     const roll = async() => {
         try {
             const response = await fetch(`https://libro-de-los-juegos-server.onrender.com/games/doblet/table/${instance}/play`, {
@@ -56,6 +71,7 @@ function Doblet() {
             <div id='main'>
                 <Board board={board}></Board>
                 <button onClick={roll}>Roll!</button>
+                <button onClick={quit}>Quit</button>
             </div>
         </div>
     )
